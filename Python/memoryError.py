581a6afc6dd774844e48d3da4fc4eb06be82abc8
Mason Fisher <02mason.fisher@gulllakecs.org>
2019-12-18 14:11:13 -0500

    Made memoryerror test

---
+++ Python/memoryError.py
@@ -0,0 +1,6 @@
+# WARNING, DON'T RUN THIS UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+FLOTA = 2 # Four lines of the apocalypse
+while True:
+    FLOTA = FLOTA**2 # Exponent
+    print(FLOTA) # Print heck
