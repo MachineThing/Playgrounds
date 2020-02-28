26afd9dd77d427f9b320694a2fa1e7f4853de5b9
Mason Fisher <sackzombie10@gmail.com>
2020-02-27 22:44:16 -0500

    Added a package to my 'random' application

--- Rust/random/src/main.rs
+++ Rust/random/src/main.rs
@@ -1,3 +1,7 @@
+use std::io;    // Standard Input Output crate
+use rand::Rng;  // Random Number Generator crate
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess my secret number it\'s between 1 and 1000!");
+    println!("How fast can you guess my number?")
 }
