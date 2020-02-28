5c978a592d52c33040c84d29334a7ba8d255a9da
Mason Fisher <sackzombie10@gmail.com>
2020-02-27 23:00:08 -0500

    Made the app generate a random number

--- Rust/random/src/main.rs
+++ Rust/random/src/main.rs
@@ -3,5 +3,9 @@ use rand::Rng;  // Random Number Generator crate
 
 fn main() {
     println!("Guess my secret number it\'s between 1 and 1000!");
-    println!("How fast can you guess my number?")
+    println!("How fast can you guess my number?");
+
+    let secret_number = rand::thread_rng().gen_range(1, 1001); // A random number between 1 and 1000
+
+    println!("The secret number is: {}", secret_number);
 }
