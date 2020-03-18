8ab79743e655623e8e0f7f7ab7b3a3b83c402b34
Mason Fisher <sackzombie10@gmail.com>
2020-03-17 20:25:19 -0400

    Made a message box appear!

--- C/WindowsApi/window.c
+++ C/WindowsApi/window.c
@@ -1,6 +1,6 @@
 #include <windows.h> // Windows API
 
-int main() {
-
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int nShowCmd) {
+  MessageBox(NULL, "Hello!", "Hello there!", MB_HELP | MB_ICONINFORMATION | MB_DEFAULT_DESKTOP_ONLY);
   return 0;
 }
