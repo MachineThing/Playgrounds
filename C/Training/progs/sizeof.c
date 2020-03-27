5134dbce5a0433beda77d91eb5ead3bc0ad5656e
Mason Fisher <sackzombie10@gmail.com>
2020-03-27 13:52:25 -0400

    Added more comments

--- C/Training/progs/sizeof.c
+++ C/Training/progs/sizeof.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> // STandarD Input Output library
 
-int main() {
+int main() { // Entry point
   printf("Size of data types in bytes:\n");
-  printf("int: %d \n", sizeof(int));
-  printf("float: %d \n", sizeof(float));
-  printf("double: %d \n", sizeof(double));
-  printf("char: %d \n", sizeof(char));
+  printf("int: %d \n", sizeof(int));        // The size of an int in bytes
+  printf("float: %d \n", sizeof(float));    // The size of a float in bytes
+  printf("double: %d \n", sizeof(double));  // The size of a double in bytes
+  printf("char: %d \n", sizeof(char));      // The size of a character in bytes
 
-  return 0;
+  return 0; // Return the 0 exit code
 }
