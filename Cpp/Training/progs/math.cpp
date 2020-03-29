bd49870e528db099a9b00dde68353fd204114538
Mason Fisher <sackzombie10@gmail.com>
2020-03-28 21:54:49 -0400

    Did math with C++

---
+++ Cpp/Training/progs/math.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Input Output STREAM library
+using namespace std; // Use the STandarD namespace
+
+int main() { // Entry point
+  const int numa = 17; // constant intiger variable
+  const int numb = 42; // constant intiger variable
+  std::cout << numa << " plus " << numb << " equals " << numa+numb << "!" << endl;
+  return 0; // Return 0 as the exit code
+}
